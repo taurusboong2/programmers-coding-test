9d83fc6718f08f6a918bf0887df1080fe9f7fd41
boong <taurusboong2@gmail.com>
2022-12-06 21:43:37 +0900

    feat: 두 수의 차

---
+++ Lv0/두수의차.js
@@ -0,0 +1,6 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+// num1이 2이고 num2가 3이므로 2 - 3 = -1을 return합니다.
+// num1이 100이고 num2가 2이므로 100 - 2 = 98을 return합니다.
+
+const solution = (num1, num2) => num1 - num2;
