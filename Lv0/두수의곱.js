14ec71e487a220db599d206545c4f152ccde37eb
boong <taurusboong2@gmail.com>
2022-12-06 21:47:58 +0900

    feat: 두 수의 곱

---
+++ Lv0/두수의곱.js
@@ -0,0 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+const solution = (num1, num2) => num1 * num2;
