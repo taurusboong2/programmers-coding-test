e0d7c3f306e28b20aea82db62719d6fd574fdc17
boong <taurusboong2@gmail.com>
2022-12-08 22:20:02 +0900

    feat: 두 수의 합

---
+++ Lv0/두수의합.js
@@ -0,0 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+const solution = (num1, num2) => num1 + num2;
