def31a89e33fa4002ff37a668b06230648ed18c6
boong <taurusboong2@gmail.com>
2022-12-20 20:45:41 +0900

    feat: 피자 나눠먹기1

---
+++ Lv0/피자나눠먹기1.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120814
+
+// 머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+
+const solution = (n) => Math.ceil(n / 7);
