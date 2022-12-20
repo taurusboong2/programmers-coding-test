a58615a93afcb08b54d4aa7191701c7944c6d807
boong <taurusboong2@gmail.com>
2022-12-20 20:45:24 +0900

    feat: 배열 두 배 만들기

---
+++ Lv0/배열두배만들기.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+// 정수 배열 `numbers`가 매개변수로 주어집니다. `numbers`의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+const solution = (numbers) => numbers.map((num) => num * 2);
