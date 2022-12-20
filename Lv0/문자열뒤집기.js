9a77de709e45d538c4f5c95a29d3b5e7588e5d04
boong <taurusboong2@gmail.com>
2022-12-20 20:46:20 +0900

    feat: 문자열 뒤집기

---
+++ Lv0/문자열뒤집기.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120822
+
+// 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+
+const solution = (str) => [...str].reverse().join('');
