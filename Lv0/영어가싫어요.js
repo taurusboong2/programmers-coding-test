30f093f271f8a129822490d52a8389d6b8b56a4a
boong <taurusboong2@gmail.com>
2023-02-25 21:21:10 +0900

    feat: 영어가 싫어요

---
+++ Lv0/영어가싫어요.js
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120894
+
+// 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다. 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+
+function solution(numbers) {
+  const obj = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const regex = new RegExp(Object.keys(obj).join('|'), 'g');
+  return Number(numbers.replace(regex, (key) => obj[key]));
+}
