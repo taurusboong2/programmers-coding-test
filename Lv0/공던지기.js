9905d8409b5858bcfef89979c1918058b4d56f19
boong <taurusboong2@gmail.com>
2023-03-06 21:52:01 +0900

    feat: 공던지기

--- Lv0/공던지기.js
+++ Lv0/공던지기.js
@@ -1,10 +1,8 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/120913]
+// https://school.programmers.co.kr/learn/courses/30/lessons/120843
 
-// 문자열 `my_str`과 `n`이 매개변수로 주어질 때, `my_str`을 길이 `n`씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+// 머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다. 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다. 친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때, k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
 
-const solution = (my_str, n) => {
-  const sliceString = (str, n) =>
-    str.length > 0 ? [str.slice(0, n), ...sliceString(str.slice(n), n)] : [];
-
-  return sliceString(my_str, n);
+const solution = (numbers, k) => {
+  const numBalls = numbers.length;
+  return (((k - 1) * 2) % numBalls) + 1;
 };
